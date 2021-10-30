373211219035e5a75f2072e1fb43865b1f76a688
yanagiya-umi(imac) <yana3umi@gmail.com>
2021-10-30 23:49:06 +0900

    [add]users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   get 'homes/top'
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
